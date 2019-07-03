58aaf0dc5617d17306b70289bf5d0167d459f95b
Ben Cross <ben_cross@me.com>
2019-07-03 16:50:36 +0100

    what can do wrong v2

---
+++ script.js
@@ -0,0 +1,42 @@
+const images = [
+  "images/image-1.png", "images/image-2.png", "images/image-3.png"
+]
+
+console.log(images[0])
+
+let i = 0
+
+function placeImage(x, y) {
+
+  const nextImage = images[i]
+
+  const img = document.createElement("img")
+
+  img.setAttribute("src", nextImage)
+  img.style.left = x + "px"
+  img.style.top = y + "px"
+  img.style.transform = "translate(-50%, -50%) scale(0.5) rotate(" + (Math.random() * 20 - 10)+ "deg)"
+
+
+
+
+  document.body.appendChild(img)
+
+  i = i + 1
+
+  if (i>= images.length) {
+    i = 0
+  }
+
+}
+
+
+document.addEventListener("click", function(event) {
+  event.preventDefault()
+  placeImage(event.pageX, event.pageY)
+})
+
+document.addEventListener("touchend", function(event) {
+  event.preventDefault()
+  placeImage(event.pageX, event.pageY)
+})
