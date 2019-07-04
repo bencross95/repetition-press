5c65a8de4bcb76ea6f52bc8c212e91ef70898b7d
Ben Cross <ben_cross@me.com>
2019-07-04 15:52:36 +0100

    drag and click working

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const images = [
   "images/image-1.png", "images/image-2.png", "images/image-3.png"
 ]
 
-console.log(images[0])
-
-let i = 0
+let i = 0;
+let justDragged = false;
 
 function placeImage(x, y) {
 
@@ -28,15 +27,106 @@ function placeImage(x, y) {
     i = 0
   }
 
+  dragElement(img)
+
+
 }
 
 
+
+
 document.addEventListener("click", function(event) {
-  event.preventDefault()
-  placeImage(event.pageX, event.pageY)
+  if (justDragged == false) {
+    event.preventDefault()
+    placeImage(event.pageX, event.pageY)
+  }
+  justDragged = false;
 })
 
+
+
+
 document.addEventListener("touchend", function(event) {
-  event.preventDefault()
-  placeImage(event.pageX, event.pageY)
+    if (justDragged == false) {
+      event.preventDefault()
+      placeImage(event.pageX, event.pageY)
+    }
+  justDragged = false;
 })
+
+
+
+
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+  elmnt.onmousedown = dragMouseDown;
+
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+    justDragged = true;
+    console.log('I have just dragged');
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+    // console.log(pos3);
+
+
+    // if (pos3 < 1) {
+    //   console.log("oioi");
+    // }
+
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+    onBrag = false;
+
+
+    // console.log("closeDragElement");
+
+
+  }
+
+
+    // When you click, and closeDragElement doesnt run, place image
+
+
+
+    // Either this function runs, or the other one doesnt
+    // if this closeDragElement runs, the placeImage doesn't
+
+
+}
+
+document.getElementsByTagName("html")[0].style.cursor = "crosshair"
+
+if (document.getElementsByTagName("html")[0].style.cursor = "crosshair") {
+  console.log("fucking yes boy")
+}
+
+
+console.log(document.getElementsByTagName("html")[0].style.cursor);
+// document.getElementsByTagName('p');
