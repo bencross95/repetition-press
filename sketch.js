2629a441c520d21282065ea7c937b51cc0a1891d
Ben Cross <ben_cross@me.com>
2020-04-21 08:27:57 +0100

    V0.5

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
-// function setup() {
-//   let myCanvas = createCanvas(300, 300);
-//   myCanvas.parent('header');
-// }
-//
-// function draw() {
-//   ellipse(mouseX, mouseY, 80, 80);
-// }
+function setup() {
+  let myCanvas = createCanvas(windowWidth, windowHeight);
+  myCanvas.parent('header');
+}
+
+function draw() {
+  let c = color(255, 204, 0);
+  fill(c);
+  noStroke();
+  ellipse(mouseX, mouseY, 80, 80);
+}
+
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
