5cb4677ddb10075e59acb53552c95902e4cc3469
Ben Cross <ben_cross@me.com>
2020-04-21 09:16:35 +0100

    added some 3D wack

--- sketch.js
+++ sketch.js
@@ -1,15 +1,37 @@
+
+
 function setup() {
-  let myCanvas = createCanvas(windowWidth, windowHeight);
+  let myCanvas = createCanvas(windowWidth, windowHeight, WEBGL);
   myCanvas.parent('header');
 }
 
 function draw() {
-  let c = color(255, 204, 0);
-  fill(c);
-  noStroke();
-  ellipse(mouseX, mouseY, 80, 80);
-}
 
+  // background(205, 105, 9);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  ellipsoid(200, 200, 40, 12);
+
+
+  // let c = color(255, 204, 0);
+  // let white = color(255, 255, 255, 0);
+  // // fill(c);
+  // noFill();
+  // stroke(c);
+  // rectMode(CENTER);
+  // ellipse(windowWidth/2, windowHeight/2, mouseX, mouseY);
+  //
+  // stroke(200, 80, 0);
+  // strokeWeight(2);
+  //
+  //
+  // if (mouseIsPressed) {
+  //   stroke(white);
+  // } else {
+  //   stroke(c);
+  // }
+
+}
 
 
 function windowResized() {
